532d92354cb9fab360845e56821b61ad29795e6d
doojin <dmitry.papka@gmail.com>
2020-09-02 13:05:21 -0500

    moving the player around with WASD keys

--- app.js
+++ app.js
@@ -10,6 +10,31 @@
         }
     };
 
+    window.addEventListener('keypress', ({ keyCode }) => {
+        const KEYCODE_A = 97;
+        const KEYCODE_W = 119;
+        const KEYCODE_S = 115;
+        const KEYCODE_D = 100;
+
+        const MOVEMENT_DELTA = 100;
+
+        if (keyCode === KEYCODE_A) {
+            updatePlayerPosition(state.player.x - MOVEMENT_DELTA, state.player.y);
+        }
+
+        if (keyCode === KEYCODE_W) {
+            updatePlayerPosition(state.player.x, state.player.y - MOVEMENT_DELTA);
+        }
+
+        if (keyCode === KEYCODE_S) {
+            updatePlayerPosition(state.player.x, state.player.y + MOVEMENT_DELTA);
+        }
+
+        if (keyCode === KEYCODE_D) {
+            updatePlayerPosition(state.player.x + MOVEMENT_DELTA, state.player.y);
+        }
+    });
+
     function updatePlayerPosition(x, y) {
         state.player.x = x;
         state.player.y = y;
