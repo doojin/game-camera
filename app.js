7a81806fd0e48161da5117ac200666105ed677e2
doojin <dmitry.papka@gmail.com>
2020-09-02 12:47:10 -0500

    drawing player icon

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 (async () => {
+    const state = {
+        camera: {
+            x: 0,
+            y: 0
+        },
+        player: {
+            x: 0,
+            y: 0
+        }
+    };
+
+    function updatePlayerPosition(x, y) {
+        state.player.x = x;
+        state.player.y = y;
+
+        state.camera.x = x;
+        state.camera.y = y;
+    }
+    
+    const playerImage = new Image();
+    playerImage.src = './player.svg';
+    await new Promise(resolve => {
+        playerImage.onload = resolve;
+    });
+
     const mapResource = await fetch('./map.json');
     const map = await mapResource.json();
 
@@ -15,6 +40,7 @@
 
     window.addEventListener('resize', resizeCallback);
     resizeCallback();
+    updatePlayerPosition(gameCanvas.width / 2, gameCanvas.height / 2);
 
     function drawCanvas() {
         context.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
@@ -24,6 +50,8 @@
             context.rect(object.x, object.y, object.w, object.h);
             context.stroke();
         });
+
+        context.drawImage(playerImage, state.player.x, state.player.y);
 
         window.requestAnimationFrame(drawCanvas);
     }
