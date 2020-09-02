ff5bdb2c98c348026a7ee05eb3092f257d4be109
doojin <dmitry.papka@gmail.com>
2020-09-02 12:29:45 -0500

    drawing map object on canvas

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hello, world!');
+(async () => {
+    const mapResource = await fetch('./map.json');
+    const map = await mapResource.json();
+
+    const gameCanvas = document.querySelector('.game');
+    const context = gameCanvas.getContext('2d');
+
+    function drawCanvas() {
+        context.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+        map.objects.forEach(object => {
+            context.beginPath();
+            context.rect(object.x, object.y, object.w, object.h);
+            context.stroke();
+        });
+
+        window.requestAnimationFrame(drawCanvas);
+    }
+
+    window.requestAnimationFrame(drawCanvas);
+
+})();
